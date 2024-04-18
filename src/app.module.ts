1369246ad37dbf03595f64b4027d61b2c90d5f1f
vinicius-francozo <vinicius.francozo@pedbot.com.br>
2024-04-18 16:09:31 -0300

    adding typeorm, and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,36 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { BooksModule } from './books/books.module';
-import { CategoriesModule } from './categories/categories.module';
-import { FavoritesModule } from './favorites/favorites.module';
-import { RentsModule } from './rents/rents.module';
-import { ReviewsModule } from './reviews/reviews.module';
-import { AuthorsModule } from './authors/authors.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { BooksModule } from "./books/books.module";
+import { CategoriesModule } from "./categories/categories.module";
+import { FavoritesModule } from "./favorites/favorites.module";
+import { RentsModule } from "./rents/rents.module";
+import { ReviewsModule } from "./reviews/reviews.module";
+import { AuthorsModule } from "./authors/authors.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [UsersModule, BooksModule, CategoriesModule, FavoritesModule, RentsModule, ReviewsModule, AuthorsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    BooksModule,
+    CategoriesModule,
+    FavoritesModule,
+    RentsModule,
+    ReviewsModule,
+    AuthorsModule,
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
